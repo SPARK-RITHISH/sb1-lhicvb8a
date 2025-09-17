6140f9e743b9ae75c0d1e441c03254547be3d97b
SPARK-RITHISH <rithishsekar421@gmail.com>
2025-09-17 21:20:51 +0530

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar: React.FC<NavbarProps> = ({ toggleSidebar }) => {
             </button>
             <Link to="/" className="flex items-center">
               <img 
-                src="image.png" 
+                src="collegelogo.jpg" 
                 alt="College Logo" 
                 className="h-12 w-12 mr-3"
               />
